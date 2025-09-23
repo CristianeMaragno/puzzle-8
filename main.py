b69b581d140421a1e46f520f6824b38eb49d35b2
Jenifer Ovejero <jenifersofiaovejero@gmail.com>
2025-09-23 17:35:05 -0300

    '0' como vazio

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def menu(tabuleiro):
             print("Entrada inválida. Por favor, digite um número.")
 
 def main():
-    caminho = "tabuleiro3.txt"
+    caminho = "tabuleiro2.txt"
     tabuleiro = estado_inicial(caminho)
 
     if tabuleiro:
